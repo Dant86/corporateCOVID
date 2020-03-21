664e945cd0a2bc5fe69393c44a9e87a85dd7fa24
Dant86 <vedantdpathak@iCloud.com>
2020-03-21 01:27:12 +0000

    unfucked

--- corporateCOVID/corporateCOVID/urls.py
+++ corporateCOVID/corporateCOVID/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.views.generic import TemplateView
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='corporateCOVID/index.html')),
+    path('', include('forum.urls')),
 ]

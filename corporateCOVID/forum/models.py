df02b57278863c8d9434139926efee1e8acfe4e1
Dant86 <vedantdpathak@iCloud.com>
2020-03-21 14:03:02 +0000

    models added and migrated, industry tests working

--- corporateCOVID/forum/models.py
+++ corporateCOVID/forum/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class Industry(models.Model):
+    industry_field = models.CharField(max_length=64)
+
+class Company(models.Model):
+    company_name = models.CharField(max_length=64)
+    industry_from = models.ForeignKey(
+        Industry,
+        on_delete=models.CASCADE
+    )
+
+class User(models.Model):
+    username = models.CharField(max_length=64)
+    password_hash = models.CharField(max_length=64)
+    email = models.CharField(max_length=64)
+
+class Post(models.Model):
+    body = models.TextField()
+    timestamp = models.DateTimeField()
+    company_about = models.ForeignKey(
+        Company,
+        on_delete=models.CASCADE,
+        null=True
+    )
+    posted_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+class Comment(models.Model):
+    body = models.TextField()
+    timestamp = models.DateTimeField()
+    is_positive = models.BooleanField()
+    commented_on = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE
+    )
+    commented_by = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE
+    )
+
--- corporateCOVID/forum/tests.py
+++ corporateCOVID/forum/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.core.exceptions import ObjectDoesNotExist
+from forum.models import Industry, Company, Post, User, Comment
 
-# Create your tests here.
+class IndustryTestCase(TestCase):
+    def setUp(self):
+        Industry.objects.create(industry_field='Aviation')
+        Industry.objects.create(industry_field='Technology')
+
+    def test_existence(self):
+        # check to see if objects exist
+        aviation = Industry.objects.get(industry_field='Aviation')
+        tech = Industry.objects.get(industry_field='Technology')
+
+        # make sure names aren't corrupted
+        self.assertEqual(aviation.industry_field, 'Aviation')
+        self.assertEqual(tech.industry_field, 'Technology')

ed3b6fe4460cf219f44e03c9e99153f207add99e
Dant86 <vedantdpathak@iCloud.com>
2020-03-21 16:03:14 +0000

    all industry health checks are added and existence health check for company is fleshed

--- corporateCOVID/forum/tests.py
+++ corporateCOVID/forum/tests.py
@@ -15,3 +15,69 @@ class IndustryTestCase(TestCase):
         # make sure names aren't corrupted
         self.assertEqual(aviation.industry_field, 'Aviation')
         self.assertEqual(tech.industry_field, 'Technology')
+
+    def test_update(self):
+        # check that db keeps track of updates
+        aviation = Industry.objects.get(industry_field='Aviation')
+        aviation.industry_field = 'Investment Banking'
+        aviation.save()
+        self.assertNotEqual(aviation.industry_field, 'Aviation')
+        self.assertEqual(aviation.industry_field, 'Investment Banking')
+        try:
+            bad_obj = Industry.objects.get(industry_field='Aviation')
+        except ObjectDoesNotExist:
+            bad_obj = None
+        self.assertIsNone(bad_obj)
+        # The below line should return an error
+        # if this doesn't exist. Expecting no error tho.
+        good_obj = Industry.objects.get(industry_field='Investment Banking')
+
+    def test_remove(self):
+        # check that db keeps track of deletions
+        aviation = Industry.objects.get(industry_field='Aviation')
+        aviation.delete()
+        self.assertEqual(len(Industry.objects.filter()), 1)
+
+class CompanyTestCase(TestCase):
+    def setUp(self):
+        # set up industries
+        IndustryTestCase().setUp()
+        
+        aviation = Industry.objects.get(industry_field='Aviation')
+        tech = Industry.objects.get(industry_field='Technology')
+
+        # add companies to db
+        Company.objects.create(company_name='American Airlines',
+                               industry_from=aviation)
+        Company.objects.create(company_name='British Airways',
+                               industry_from=aviation)
+        Company.objects.create(company_name='Amazon',
+                               industry_from=tech)
+        Company.objects.create(company_name='Apple',
+                               industry_from=tech)
+
+    def test_existence(self):
+        # check if null
+        aa = Company.objects.get(company_name='American Airlines')
+        ba = Company.objects.get(company_name='British Airways')
+        amzn = Company.objects.get(company_name='Amazon')
+        aapl = Company.objects.get(company_name='Apple')
+
+        # check if no info is corrupted
+        self.assertEqual(aa.company_name, 'American Airlines')
+        self.assertEqual(ba.company_name, 'British Airways')
+        self.assertEqual(amzn.company_name, 'Amazon')
+        self.assertEqual(aapl.company_name, 'Apple')
+
+        # check links up to industry level
+        self.assertEqual(aa.industry_from.industry_field, 'Aviation')
+        self.assertEqual(ba.industry_from.industry_field, 'Aviation')
+        self.assertEqual(amzn.industry_from.industry_field, 'Technology')
+        self.assertEqual(aapl.industry_from.industry_field, 'Technology')
+        
+        # check downward links from industry level
+        aviation_companies = Company.objects.filter(industry_from__industry_field='Aviation')
+        self.assertEqual(len(aviation_companies), 2)
+        tech_companies = Company.objects.filter(industry_from__industry_field='Technology')
+        self.assertEqual(len(tech_companies), 2)
+
